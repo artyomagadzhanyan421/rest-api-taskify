52b4f763ae042c3c31bf7a9eb971c816f01ef781
artyomagadzhanyan421 <artyom.agadzhanyan421@gmail.com>
2025-03-15 12:10:47 +0500

    users commit

--- mongodb/models/userModel.js
+++ mongodb/models/userModel.js
@@ -7,4 +7,4 @@ const userSchema = new mongoose.Schema({
     password: { type: String, required: true },
 });
 
-module.exports = mongoose.model("users", userSchema);
+module.exports = mongoose.model("User", userSchema);
